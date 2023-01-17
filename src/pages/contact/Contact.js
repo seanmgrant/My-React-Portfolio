be4eedf69a0f60ebc3d296bd7c9b16d60f167f3f
Sean Grant <104331199+seanmgrant@users.noreply.github.com>
2023-01-17 14:25:04 -0700

    added contact page

--- src/pages/contact/Contact.js
+++ src/pages/contact/Contact.js
@@ -1,10 +1,51 @@
-import React from "react";
+import React, { useState } from 'react';
+import { Form, Button } from 'react-bootstrap';
+import './contact.css';
 
+function ContactPage() {
+  const [formData, setFormData] = useState({
+    name: '',
+    email: '',
+    message: ''
+  });
 
-export default function Contact() {
-    return (
-        <div>
-        <h1>Contact</h1>
-        </div>
-    )       
+  const handleChange = (event) => {
+    setFormData({
+      ...formData,
+      [event.target.name]: event.target.value
+    });
+  }
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    // Send the form data to your server or email service here
+    alert('Form submitted!');
+  }
+
+  return (
+    <div className='container'>
+    <Form onSubmit={handleSubmit}>
+      <Form.Group >
+        <Form.Label className='name'>Name</Form.Label>
+        <Form.Control type="text" name="name" value={formData.name} onChange={handleChange} />
+      </Form.Group>
+
+      <Form.Group>
+        <Form.Label className='email'>Email</Form.Label>
+        <Form.Control type="email" name="email" value={formData.email} onChange={handleChange} />
+      </Form.Group>
+
+      <Form.Group>
+        <Form.Label className='message'>Message</Form.Label>
+        <Form.Control as="textarea" rows="3" name="message" value={formData.message} onChange={handleChange} />
+      </Form.Group>
+
+      <Button variant="primary" type="submit">
+        Submit
+      </Button>
+    </Form>
+    </div>
+  );
 }
+
+export default ContactPage;
