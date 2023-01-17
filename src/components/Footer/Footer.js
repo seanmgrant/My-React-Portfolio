36c8b5c250d43283e14010283260c40dfb2e25b9
Sean Grant <104331199+seanmgrant@users.noreply.github.com>
2023-01-17 15:39:18 -0700

    Got social links to work and size

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,8 +1,47 @@
-import React from 'react'
+import React from 'react';
+import './footer.css';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faGithub, faLinkedin } from '@fortawesome/free-brands-svg-icons';
+import { faEnvelope } from '@fortawesome/free-solid-svg-icons';
+import { Container, Row, Col  } from 'react-bootstrap';
+
 
 export default function Footer() {
   return (
-    <div>Footer</div>
-  )
-}
+    <div className='footer-basic'>
+      <footer className='container text-center'>
+      <div className='row'>
+          <a
+            className='col-sm-2 fa-3x'
+            href="https://github.com/seanmgrant"
+            target="_blank"
+            rel="noreferrer"
+          >
+            <FontAwesomeIcon icon={faGithub} />
+          </a>
+          <a
+            className='col-sm-2 fa-3x'
+            href="https://www.linkedin.com/in/sean-grant-a7858551/"
+            target="_blank"
+            rel="noreferrer"
+          >
+            <FontAwesomeIcon icon={faLinkedin} />
+          </a>
+         
+        <a
+            className='col-sm-2 fa-3x'
+            href="contact"
+            target="_blank"
+            rel="noreferrer"
+            >
+          <FontAwesomeIcon icon={faEnvelope} />
+          </a>
+     
+            </div>
+
+      </footer>
+      </div>
+    
+  );
+};
 
